64feb03ee62daa878b449acd72288637792ecb3c
kashinotcached <kashinathnagvekar1@gmail.com>
2025-02-27 12:14:40 +0530

    Add project files.

---
+++ Tests/WorldsDumbestFunctionTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Tests
+{
+    public static class WorldsDumbestFunctionTests
+    {
+        //Naming Convention - ClassName_MethodName_ExpectedResult
+        public static void WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString()
+        {
+            try
+            {
+                //Arrange - Go get your variables, your classes, your functions, whatever you need.
+                int num = 0;
+                WorldsDumbestFunction worldsDumbest = new WorldsDumbestFunction();
+
+                //Act - Execute this function
+                string result = worldsDumbest.ReturnsPikachuIfZero(num);
+
+                //Assert - Whatever is returned is what you want.
+                if(result == "PIKACHU!")
+                    Console.WriteLine("PASSED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString");
+                else
+                    Console.WriteLine("FAILED: WorldsDumbestFunction_ReturnsPikachuIfZero_ReturnString");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
